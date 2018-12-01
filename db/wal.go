1bfba97fd8ef2f05e83a8d54928b57dd49a9bc2e
Tej Chajed <tchajed@mit.edu>
2018-11-30 23:30:06 -0500

    Implement search tree for log
    Though deletes aren't correctly handled with multiple young generation
    files.

--- db/wal.go
+++ db/wal.go
@@ -6,6 +6,8 @@ package db
 // Gets from the log (missing keys may have been migrated to SSTables)
 
 import (
+	"sort"
+
 	"github.com/tchajed/specious-db/fs"
 	"github.com/tchajed/specious-db/log"
 )
@@ -17,27 +19,73 @@ type dbLog struct {
 	cache entrySearchTree
 }
 
-// TODO: implement search tree
-type entrySearchTree struct{}
+type entrySearchTree struct {
+	// works because keys are uint64s
+	cache map[Key]Value
+}
 
 func (t entrySearchTree) Get(k Key) MaybeValue {
-	panic("not implemented")
+	v, ok := t.cache[k]
+	if ok {
+		return SomeValue(v)
+	} else {
+		return NoValue
+	}
 }
 
 func (t entrySearchTree) Put(k Key, v Value) {
-	panic("not implemented")
+	t.cache[k] = v
 }
 
 func (t entrySearchTree) Delete(k Key) {
-	panic("not implemented")
+	delete(t.cache, k)
+}
+
+type inOrderIterator struct {
+	entries []Entry
+}
+
+func (it inOrderIterator) Len() int {
+	return len(it.entries)
+}
+
+func (it inOrderIterator) Less(i, j int) bool {
+	return it.entries[i].Key < it.entries[j].Key
+}
+
+func (it inOrderIterator) Swap(i, j int) {
+	it.entries[i], it.entries[j] = it.entries[j], it.entries[i]
+}
+
+// Create an iterator over a list of entries and put it in order;
+// consumes the input slice (because it's sorted in-place).
+func sortedIterator(entries []Entry) *inOrderIterator {
+	it := inOrderIterator{entries}
+	sort.Sort(it)
+	return &it
+}
+
+func (it inOrderIterator) IsDone() bool {
+	return len(it.entries) == 0
+}
+
+func (it *inOrderIterator) Next() Entry {
+	e := it.entries[0]
+	it.entries = it.entries[1:]
+	return e
 }
 
 func (t entrySearchTree) Stream() EntryIterator {
 	// NOTE: this stream already colaesces all updates to the same key
 	//
-	// NOTE: streaming should be in-order, everything else relies on this
-	// initial ordering
-	panic("not implemented")
+	// TODO: this is actually insufficient; need to record deletions so two log
+	// files can be concatenated (though recording only deletions and clearing
+	// the deletions on Put is sufficient)
+	entries := make([]Entry, 0, len(t.cache))
+	for k, v := range t.cache {
+		entries = append(entries, Entry{k, v})
+	}
+	return sortedIterator(entries)
 }
 
 func (l dbLog) Get(k Key) MaybeValue {
