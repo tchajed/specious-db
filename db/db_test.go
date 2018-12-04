f04d7d9f968068a41c915dfe14798f916f3d30dd
Tej Chajed <tchajed@mit.edu>
2018-12-04 14:42:12 -0500

    Add tracking of expected DB results

--- db/db_test.go
+++ db/db_test.go
@@ -1,13 +1,18 @@
 package db
 
 import (
+	"fmt"
+
 	"github.com/stretchr/testify/suite"
 	"github.com/tchajed/specious-db/fs"
 )
 
 const missing = "<missing>"
 
-type StringStore struct{ *Database }
+type StringStore struct {
+	*Database
+	gold map[int]string
+}
 
 func (s StringStore) Get(k int) string {
 	v := s.Database.Get(Key(k))
@@ -17,11 +22,21 @@ func (s StringStore) Get(k int) string {
 	return missing
 }
 
+func (s StringStore) Expected(k int) string {
+	v, ok := s.gold[k]
+	if ok {
+		return v
+	}
+	return missing
+}
+
 func (s StringStore) Put(k int, v string) {
 	if v == missing {
 		s.Database.Delete(Key(k))
+		delete(s.gold, k)
 	} else {
 		s.Database.Put(Key(k), []byte(v))
+		s.gold[k] = v
 	}
 }
 
@@ -33,5 +48,15 @@ type DbSuite struct {
 
 func (suite *DbSuite) SetupTest() {
 	suite.fs = fs.MemFs()
-	suite.db = StringStore{Init(suite.fs)}
+	suite.db = StringStore{Init(suite.fs), make(map[int]string)}
+}
+
+func (suite *DbSuite) putValues(min, max int) {
+	for i := min; i <= max; i++ {
+		suite.db.Put(i, fmt.Sprintf("val %d", i))
+	}
+}
+
+func (suite *DbSuite) checkKey(key int, msgAndArgs ...interface{}) {
+	suite.Equal(suite.db.Expected(key), suite.db.Get(key), msgAndArgs...)
 }
