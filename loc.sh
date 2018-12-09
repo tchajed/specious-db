b266435bbd0a04682481033e50d38dbdb37b317d
Tej Chajed <tchajed@mit.edu>
2018-12-09 13:37:03 -0500

    Make tail call more portable

--- loc.sh
+++ loc.sh
@@ -7,6 +7,6 @@ set -e
 find . -name '*_test.go' > .ignored-files.txt
 
 CLOC="cloc --quiet --hide-rate --by-file"
-$CLOC --include-ext='go' --exclude-list-file=.ignored-files.txt db bin log fs/fs.go | tail +2
+$CLOC --include-ext='go' --exclude-list-file=.ignored-files.txt db bin log fs/fs.go | tail -n +2
 
 rm .ignored-files.txt
